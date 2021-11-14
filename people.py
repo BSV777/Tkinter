fa8e50703048278179104c07ec862f4db35b92dc
Sergey Baykov <baykov-sv@yandex.ru>
2021-11-14 19:46:03 +0300

    Заготовка для задания

--- people.py
+++ people.py
@@ -12,11 +12,19 @@ c.pack()
 
 
 
-oval = c.create_oval(20, 20, 40, 40)
+class Man:
+    def __init__(self, x, y, h):
+        head = c.create_oval(x - h / 10, y, x + h / 10, y + h / 5)
+        body = c.create_oval(x - h / 10, y + h / 5, x + h / 10, y + 3 * h / 5)
+        footl = c.create_line(x - 5, y + 3 * h / 5, x - 20, y + h)
+        footr = c.create_line(x + 5, y + 3 * h / 5, x + 20, y + h)
+        handl = c.create_line(x - 8, y + h / 5 + 5, x - 30, y + 3 * h / 5)
+        handr = c.create_line(x + 8, y + h / 5 + 5, x + 30, y + 3 * h / 5)
 
-line = c.create_line(50, 50, 70, 70)
 
-
+man1 = Man(50, 50, 100)
+man2 = Man(100, 50, 120)
+man3 = Man(150, 50, 70)
 
 
 
